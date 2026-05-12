79ce7400a70eb4c20145fcacf1414242c4cd053f
MuriloMarch05 <murilomarchiori17@gmail.com>
2026-05-12 08:41:42 -0300

    Adiciona programa que lê uma matriz 4x4 e conta elementos maiores que 15

---
+++ Aula 12/01_matriz4x4.cpp
@@ -0,0 +1,37 @@
+/*1) Seja a matriz inteira valores (4x4). Preenchê-la por leitura, apresentar seus valores, e a
+quantidade de elementos maiores que 15.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int matriz[4][4], qtde=0;
+    int l, c;
+    
+    cout << "Preencha a matriz 4x4 com inteiros: " << endl;
+    
+    for(l=0;l<4;l++)
+    {
+        for(c=0;c<4;c++)
+        {
+            cin >> matriz[l][c];
+            
+            if (matriz[l][c] > 15)
+            {
+                qtde++;
+            }
+        }
+    }
+    
+    cout << "Os valores digitados foram: " << endl;
+    for(l=0;l<4;l++)
+    {
+        for(c=0;c<4;c++)
+        {
+            cout << matriz[l][c] << endl;
+        }
+    }
+    
+    cout << "Quantidade de valores maiores que 15: " << qtde << endl;
+}
