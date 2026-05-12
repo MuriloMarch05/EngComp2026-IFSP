60d9aa9c10e69aacda975d0210d8ced2e93f28d7
MuriloMarch05 <murilomarchiori17@gmail.com>
2026-05-11 21:05:31 -0300

    Adiciona funcionalidade para o usuário escolher intervalo e tipo de números a serem exibidos

--- Aula 9/08_numerospares.cpp
+++ Aula 9/08_numerospares.cpp
@@ -17,7 +17,44 @@ int main()
         }
     }
 
+    // Agora, vamos deixar o usuário escolher o intervalo para exibir os números pares, impares ou todos. Para isso, vamos usar o for aninhado.
+
+    int inicio, fim;
+    cout << "Digite o início do intervalo: ";
+    cin >> inicio;
+    cout << "Digite o fim do intervalo: ";
+    cin >> fim;
+
+    cout << "Você quer exibir pares, impares ou todos os números? (P/I/T): ";
+    char escolha;
+    cin >> escolha;
+    escolha = toupper(escolha); // Convertendo para maiúscula para facilitar a comparação
+
+    cout << "Números:\n";
+    for (i = inicio;i<=fim; i++)
+    {
+        switch(escolha)
+        {
+            case 'P':
+                if(i%2 == 0)
+                {
+                    cout << i << "\n";
+                }
+                break;
+            case 'I':
+                if(i%2 != 0)
+                {
+                    cout << i << "\n";
+                }
+                break;
+            case 'T':
+                cout << i << "\n";
+                break;
+            default:
+                cout << "Opção inválida." << endl;
+                return 1; // Encerra o programa com código de erro
+        }
+    }
+
     return 0;
 }   
-
-
