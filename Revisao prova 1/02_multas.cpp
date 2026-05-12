e877c1a6ecb3637d507d21d99e374dd59ccaddbf
MuriloMarch05 <murilomarchiori17@gmail.com>
2026-05-12 13:58:16 -0300

    Add program to calculate traffic fines and total revenue for DETRAN
    - Implemented a C++ program that reads driver's license numbers, number of fines, and fine amounts.
    - Calculates and displays the total debt for each driver and the overall revenue from fines.
    - Identifies the driver with the highest number of fines.
    - Program terminates when a driver's license number of 0 is entered.

---
+++ Revisao prova 1/02_multas.cpp
@@ -0,0 +1,57 @@
+/*2) Faça um programa que ajude o DETRAN a saber o total de recursos que serão
+arrecadados com a aplicação de multas de trânsito. O programa deve ler as seguintes
+informações para cada motorista:
+ O número da carteira de motorista (de 1 a 4327)
+ O número de multas
+ O valor de cada uma das multas
+Deve ser impresso o valor da dívida para cada motorista e, ao final da leitura, o total de
+recursos arrecadados (somatório de todas as multas). O programa deverá imprimir
+também o número da carteira do motorista que obteve o maior número de multas. O
+programa termina ao ler a carteira de motorista de valor 0.*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    cout << fixed << setprecision(2);
+    int carteira, qtdMultas, maiorQtd = -1, carteiraMaior;
+    double valorMulta, somaMotorista, totalArrecadado = 0.0;
+
+    while (carteira != 0)
+    {
+        somaMotorista = 0.0; // Reinicia a soma para cada motorista
+        cout << "Digite o número da carteira (1 a 4327, ou 0 para encerrar): ";
+        cin >> carteira;
+
+        if(carteira == 0)
+        {
+            break;
+        }
+
+        cout << "Quantas multas? ";
+        cin >> qtdMultas;
+
+        if (qtdMultas > maiorQtd)
+        {
+            maiorQtd = qtdMultas;
+            carteiraMaior = carteira;
+        }
+
+        for (int i = 0; i<qtdMultas; i++)
+        {
+            cout << "Valor da multa " << (i + 1) << ": R$ ";
+            cin >> valorMulta;
+            somaMotorista += valorMulta; // Acumula o valor das multas para o motorista atual
+        }
+
+        totalArrecadado += somaMotorista; // Acumula o valor total arrecadado
+        cout << "Dívida do motorista " << carteira << ": R$ " << somaMotorista << endl;        
+    }
+
+        cout << "Total arrecadado com multas: R$ " << totalArrecadado << endl;
+        cout << "Motorista com maior número de multas: " << carteiraMaior << " (Quantidade: )" << maiorQtd << ")" << endl;
+
+    return 0;
+}
