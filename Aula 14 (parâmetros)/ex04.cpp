b378bb957fc98491b6da7d36ce6658eb20dc475a
MuriloMarch05 <murilomarchiori17@gmail.com>
2026-06-01 17:37:09 -0300

    Adiciona função para calcular a média aritmética de notas em um vetor
    - Implementa a função calcularMedia que recebe um vector<float> e retorna a média das notas.
    - Adiciona interação no main para entrada de notas pelo usuário.
    - Formata a saída da média com duas casas decimais.

---
+++ Aula 14 (parâmetros)/ex04.cpp
@@ -0,0 +1,43 @@
+/*4) Escreva uma função que receba um vector<float> com notas de alunos e retorne a média
+aritmética (retorno do tipo float).*/
+
+#include <iostream>
+#include <vector> // Para usar o tipo vector
+#include <iomanip>
+using namespace std;
+
+// Função para calcular a média das notas
+float calcularMedia(const vector<float> &notas)
+{
+    float soma = 0.0;
+    for (float nota : notas) // Percorre cada nota no vector e acumula a soma
+    {
+        soma += nota;
+    }
+
+    return notas.empty() ? 0.0 : soma / notas.size(); // 
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    vector<float> notas;
+    int quantidade;
+
+    cout << "Digite a quantidade de notas: ";
+    cin >> quantidade;
+
+    for (int i = 0; i<quantidade; i++)
+    {
+        float nota;
+        cout << "Digite a nota do aluno " << (i+1) << ": ";
+        cin >> nota;
+        notas.push_back(nota);
+    }
+
+    float media = calcularMedia(notas);
+    cout << fixed << setprecision(2);
+    cout << "A média das notas é: " << media << endl;
+
+    return 0;
+}
