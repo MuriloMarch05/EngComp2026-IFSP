aa870ce77fa3290119449277ee57e15ace053ed2
Murilo Marchiori <murilomarchiori17@gmail.com>
2026-08-24 14:23:09 -0300

    feat: adiciona implementação do catálogo de produtos com cadastro e ordenação por Selection Sort

--- BRIEDAD/Métodos de Ordenação & Ponteiros/Exercícios/2.catalogo_de_peças.cpp
+++ BRIEDAD/Métodos de Ordenação & Ponteiros/Exercícios/2.catalogo_de_peças.cpp
@@ -12,6 +12,8 @@ Uso de Ponteiro: Na lógica de troca (swap), utilize uma variável auxiliar do t
 */
 
 #include <iostream>
+#include <clocale>
+
 using namespace std;
 
 struct Produto{
@@ -19,19 +21,79 @@ struct Produto{
     double preco;
 };
 
-void cadastroProdutos(){
 
+void cadastroProdutos(Produto *p, int tam){
+
+    for(int i = 0; i < tam; i++){
+
+        cout << "Código do produto[" << i << "]: ";
+        cin >> p[i].codigo;
+
+        cout << "Preço do produto[" << i << "]: ";
+        cin >> p[i].preco;
+    }
+}
+
+
+void selectionSort(Produto *p, int tam){
+
+    int i, j, min, localTroca, trocou;
+    Produto aux;
+
+    for(i = 0; i < tam - 1; i++){
+
+        min = p[i].codigo;
+        localTroca = i;
+        trocou = 0;
+
+        for(j = i + 1; j < tam; j++){
+
+            if(p[j].codigo < min){
+
+                min = p[j].codigo;
+                localTroca = j;
+                trocou = 1;
+            }
+        }
+
+        if(trocou){
+
+            aux = p[i];
+            p[i] = p[localTroca];
+            p[localTroca] = aux;
+        }
+    }
+}
+
+
+void catalogo(Produto *p, int tam){
+
+    cout << "===== CATÁLOGO DE PRODUTOS =====" << endl;
+
+    for(int i = 0; i < tam; i++){
+
+        cout << "Produto[" << i << "]: "
+             << p[i].codigo << endl;
+
+        cout << "Preço[" << i << "]: "
+             << p[i].preco << endl;
+    }
 }
 
 
 int main(){
+
     setlocale(LC_ALL, "Portuguese");
 
-    int tam = 5;
-    Produto listaProdutos[tam];
-    
-    
+    const int tam = 5;
 
+    Produto listaProdutos[tam];
+
+    cadastroProdutos(listaProdutos, tam);
+
+    selectionSort(listaProdutos, tam);
+
+    catalogo(listaProdutos, tam);
 
     return 0;
 }
