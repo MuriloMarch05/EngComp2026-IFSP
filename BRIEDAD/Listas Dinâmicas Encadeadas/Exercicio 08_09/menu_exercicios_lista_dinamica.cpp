fc7b5edadb35fb216c6eb3d73b6bd951eb6f9a51
MuriloMarch05 <murilomarchiori17@gmail.com>
2026-09-12 14:50:13 -0300

    feat: adiciona funções para liberar, trocar valores e dividir listas, além de melhorias no menu

--- BRIEDAD/Listas Dinâmicas Encadeadas/Exercicio 08_09/menu_exercicios_lista_dinamica.cpp
+++ BRIEDAD/Listas Dinâmicas Encadeadas/Exercicio 08_09/menu_exercicios_lista_dinamica.cpp
@@ -72,6 +72,22 @@ Lista* Criar_Lista(){ // Cria a lista na memória e inicializa
 	return L;
 }
 
+void Liberar_Lista(Lista *L){
+	Item *Atual;
+	Item *Prox;
+
+	Atual = L->Inicio;
+
+	while(Atual != NULL){
+		Prox = Atual->Prox;
+		free(Atual);
+		Atual = Prox;
+	}
+
+	L->Inicio = NULL;
+
+}
+
 Item* Criar_Item(int Valor){ // Cria o item na memória e grava o valor
 	Item *Elemento; // Ponteiro para o novo item
 	
@@ -149,6 +165,7 @@ int Contar_Valor(Lista *L, int N){ // Conta quantas vezes um valor aparece na li
 		if (Atual->Valor == N){ // Se o valor do item atual for igual a N, incrementa o contador
 			cont++;
 		}
+		Atual = Atual->Prox;
 	}
 
 	return cont;
@@ -180,32 +197,118 @@ Lista* Selecionar_Lista(Lista *L1, Lista *L2){// Permite ao usuário selecionar 
 
 void Imprimir_Lista(Lista *L){ // Imprime os elementos da lista
 	Item *Atual;// Ponteiro para percorrer a lista
-
+	
 	Atual = L->Inicio;// Inicializa o ponteiro atual como o início da lista
-
+	
 	while(Atual != NULL){
 		cout << Atual->Valor << " ";
 		Atual = Atual->Prox;// Atualiza o ponteiro atual para o próximo item
 	}
-
+	
 	cout << endl;
+	
+}
+
+void Trocar_Valor(Lista *L, int Valor, int Valor_Novo){
+	Item *Atual;
+
+	Atual = L->Inicio;
+
+	while(Atual != NULL){
+		if(Atual->Valor == Valor){
+			Atual->Valor = Valor_Novo;
+		}
+		Atual = Atual->Prox;
+	}
+
+	cout << "Lista após troca: ";
+	Imprimir_Lista(L);
 
 }
 
+void Dividir_Lista(Lista *L, int N){
+	Lista *Maiores, *Menores;
+	Item *Atual;
+
+	Maiores = Criar_Lista();
+	Menores = Criar_Lista();
+
+	Atual = L->Inicio;
+
+	while(Atual != NULL){
+
+		if(Atual->Valor > N){
+			Inserir_Item(Maiores, Atual->Valor);
+		}
+		else{
+			if (Atual->Valor < N){
+				Inserir_Item(Menores, Atual->Valor);
+			}
+		}
+		Atual = Atual->Prox;
+	}
+
+	cout << "Maiores que " << N << ": ";
+	Imprimir_Lista(Maiores);
+	cout << "Menores que " << N << ": ";
+	Imprimir_Lista(Menores);
+
+	Liberar_Lista(Maiores); // Libera os nós
+	free(Maiores); // Libera a lista
+	Liberar_Lista(Menores);
+	free(Menores);
+
+}
+
+int Existe_Valor(Lista *L, int Valor){
+	Item *Atual;
+
+	Atual = L->Inicio;
+
+	while(Atual != NULL){
+		if (Atual->Valor == Valor){
+			return 1;
+		}
+
+		Atual = Atual->Prox;
+
+	}
+	return 0;
+}
+
+Lista* Inter_Lista(Lista *L1, Lista *L2){
+	Item *Atual;
+	Lista *Resultante;
+
+	Atual = L1->Inicio;
+	Resultante = Criar_Lista();
+
+	while(Atual != NULL){
+		 // existe na outra lista e ainda não foi inserido no resultado
+		if(Existe_Valor(L2, Atual->Valor) && !Existe_Valor(Resultante, Atual->Valor)){
+			Inserir_Item(Resultante, Atual->Valor);
+		}
+
+		Atual = Atual->Prox;
+	}
+
+	return Resultante;
+}
+
 void Menu(Lista *L1, Lista *L2){// Função menu para interagir com o usuário
-	int Op, Valor;// Variáveis para armazenar a opção do usuário e o valor a ser inserido ou removido
+	int Op, Valor, Valor_Novo;// Variáveis para armazenar a opção do usuário e o valor a ser inserido ou removido
 	Lista* L;// Ponteiro para a lista selecionada
-	
+	Lista *L_Inter;
 	do{
-		system("cls");
+		system("clear");
 		cout << "----------------- TESTE DE OPERAÇÕES COM LISTAS -----------------" << endl;
 		cout << "1 - Inserir na lista (Lista 1 ou Lista 2)"<< endl;
 		cout << "2 - Excluir da lista (Lista 1 ou Lista 2)"<< endl;
 		cout << "3 - Imprimir"<< endl;
-		cout << "4 - Comprimento da lista (Lista 1 ou Lista 2)" << endl;
-		cout << "5 - Contar ocorrências (Lista 1 ou Lista 2)" << endl;
-		cout << "6-  Trocar valores (Lista 1 ou Lista 2)" << endl;
-		cout << "7 - Dividir uma lista em duas (Lista 1 ou Lista 2)"<< endl;
+		cout << "4 - Comprimento da lista" << endl;
+		cout << "5 - Contar ocorrências" << endl;
+		cout << "6 -  Trocar valores" << endl;
+		cout << "7 - Dividir uma lista em duas"<< endl;
 		cout << "8 - Intersecção das listas"<< endl;
 		cout << "0 - Sair"<< endl;
 		cout << "Opção: ";
@@ -213,7 +316,7 @@ void Menu(Lista *L1, Lista *L2){// Função menu para interagir com o usuário
 		
 		switch(Op){
 			case 1:
-				// system("cls");
+				system("clear");
 				cout << "----------------- INSERÇÃO -----------------" << endl;
 				L = Selecionar_Lista(L1, L2);
 				cout << "Digite um valor a ser inserido: ";
@@ -221,7 +324,7 @@ void Menu(Lista *L1, Lista *L2){// Função menu para interagir com o usuário
 				Inserir_Item(L, Valor);
 				break;
 			case 2:
-				// system("cls");
+				system("clear");
 				cout << "----------------- EXCLUSÃO -----------------" << endl;
 				L = Selecionar_Lista(L1, L2);
 				cout << "Digite um valor a ser excluído: ";
@@ -229,64 +332,101 @@ void Menu(Lista *L1, Lista *L2){// Função menu para interagir com o usuário
 				Remover_Item(L, Valor);
 				break;	
 			case 3:
-				// system("cls");
+				system("clear");
 				cout << "----------------- IMPRESSÃO -----------------" << endl;							
 				cout << "LISTA 1: " ;
 				Imprimir_Lista(L1);				
 				cout << endl<< "LISTA 2: ";
 				Imprimir_Lista(L2);
 				
-				// system("pause");
+				cout << endl << "Pressione ENTER para continuar...";
+				cin.ignore();   // limpa o \n deixado pelo cin anterior
+				cin.get();      // espera ENTER
 				break;
 			
 			case 4:
+				system("clear");
 				cout << "----------------- COMPRIMENTO -----------------" << endl;
 				L = Selecionar_Lista(L1, L2);
 
 				cout << "Comprimento da lista escolhida: "<< Comprimento_Lista(L) << endl;
+				
+				cout << endl << "Pressione ENTER para continuar...";
+				cin.ignore();   // limpa o \n deixado pelo cin anterior
+				cin.get();      // espera ENTER
 				break;
 
 			case 5:
+				system("clear");
 				cout << "----------------- CONTAR OCORRÊNCIAS -----------------" << endl;
 				L = Selecionar_Lista(L1, L2);
 				cout << "Digite um valor para contar as ocorrências: ";
 				cin >> Valor;
 				cout << "Quantidade de ocorrencias do valor " << Valor << " na lista escolhida: " << Contar_Valor(L, Valor) << endl;
-
+				
+				cout << endl << "Pressione ENTER para continuar...";
+				cin.ignore();   // limpa o \n deixado pelo cin anterior
+				cin.get();      // espera ENTER
+				break;
 			case 6:
+				system("clear");
+				cout << "----------------- TROCAR VALORES -----------------" << endl;
+				L= Selecionar_Lista(L1, L2);
+				cout << "Digite o valor a ser trocado: ";
+				cin >> Valor;
+				cout << "Valor novo: ";
+				cin >> Valor_Novo;
+				Trocar_Valor(L, Valor, Valor_Novo);
+				
+				cout << endl << "Pressione ENTER para continuar...";
+				cin.ignore();   // limpa o \n deixado pelo cin anterior
+				cin.get();      // espera ENTER
+				break;
 
 
 			case 7:
-				// system("cls");
+				system("clear");
 				cout << "----------------- DIVISÃO -----------------" << endl;	
 				L = Selecionar_Lista(L1, L2);	
 				cout << "Digite um valor para dividir a lista em Maiores e Menores: ";
-				cin >> Valor;		
-							
-				//Dividir_Lista(L, Valor);
+				cin >> Valor;					
+				Dividir_Lista(L, Valor);
 				
-				// system("pause");
+				cout << endl << "Pressione ENTER para continuar...";
+				cin.ignore();   // limpa o \n deixado pelo cin anterior
+				cin.get();      // espera ENTER
 				break;
+
 			case 8:	
-				//system("cls");
+				system("clear");
 				cout << "----------------- INTERSECÇãO -----------------" << endl;	
 				cout << "LISTA 1: " ;
 				Imprimir_Lista(L1);				
 				cout << endl << "LISTA 2: ";
 				Imprimir_Lista(L2);						
-				//L = Inter_Lista(L1, L2);
+				L_Inter = Inter_Lista(L1, L2);
 				cout << "Itens comuns: " ;
 				Imprimir_Lista(L);
-				//system("pause");
+
+				Liberar_Lista(L_Inter);
+				free(L_Inter);
+
+				cout << endl << "Pressione ENTER para continuar...";
+				cin.ignore();   // limpa o \n deixado pelo cin anterior
+				cin.get();      // espera ENTER
 				break;									
 			case 0:
+				system("clear");
 				cout << endl << "Sair do programa? (0 = Sim, Outra tecla = Não)" << endl;
 				cout << "Resposta: ";
 				cin >> Op;				
 				break;
 			default:
 				cout << "Opção inválida!!!" << endl;
-				//system("pause");
+
+				cout << endl << "Pressione ENTER para continuar...";
+				cin.ignore();   // limpa o \n deixado pelo cin anterior
+				cin.get();      // espera ENTER
 		}
 		
 	}while (Op != 0);
@@ -303,6 +443,11 @@ int main(){
 	Ini_Lista(L2);// Inicializa a Lista 2
 	
 	Menu(L1, L2); // Chama a função menu para interagir com o usuário
+
+	Liberar_Lista(L1);
+	free(L1);
+	Liberar_Lista(L2);
+	free(L2);
 
 	return 0;
 }
